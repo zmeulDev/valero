f8f7ea8b7a9f9cd0690bf582b3e869db0df17741
zmeul-mini <fabian.hanu@gmail.com>
2024-10-04 22:37:15 +0300

    logo bug

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -2,4 +2,4 @@
 use App\Models\Setting;
 @endphp
 
-<img src="{{ asset('brand/logo.png') }}?v={{ Setting::get('logo_version', '1') }}" alt="{{ config('app.name') }} Logo" {{ $attributes ->merge(['class' => 'h-16 w-16 dark:text-white']) }}> 
+<img src="{{ asset('storage/brand/logo.png') }}?v={{ Setting::get('logo_version', '1') }}" alt="{{ config('app.name') }} Logo" {{ $attributes ->merge(['class' => 'h-16 w-16 dark:text-white']) }}> 
